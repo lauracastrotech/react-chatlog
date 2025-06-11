edecf2218bd54620da676ea753f86cd7a83fa641
Laura Castro <lauracastrotech@gmail.com>
2025-06-11 17:59:34 -0400

    adds message bubble, like icon, styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatEntryData = {
+    'sender':'Vladimir',
+    'body':'why are you arguing with me',
+    'timeStamp':'2018-05-29T22:49:06+00:00'
+  };
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry props={chatEntryData}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,28 +1,26 @@
 import './ChatEntry.css';
+import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = () => {
+const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name d-text">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p className="d-text">{props.body}</p>
+        <p className="entry-time d-text"><TimeStamp time={'2018-05-29T22:49:06+00:00'}/></p>
         <button className="like">🤍</button>
       </section>
     </div>
   );
 };
 
-ChatEntry.propTypes = {
-  // Fill with correct proptypes
-};
+// Prop validation is causing an error in console and time is hardcoded and need to debug what the data coming from app isnt passing 
+// ChatEntry.propTypes = {
+//   sender: PropTypes.string.isRequired,
+//   body: PropTypes.string.isRequired,
+//   timeStamp: PropTypes.string.isRequired
+// };
 
 export default ChatEntry;
 
-  // {
-  //   "id": 1,
-  //   "sender":"Vladimir",
-  //   "body":"why are you arguing with me",
-  //   "timeStamp":"2018-05-29T22:49:06+00:00",
-  //   "liked": false
-  // },
